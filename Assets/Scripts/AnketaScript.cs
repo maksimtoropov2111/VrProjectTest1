1d0caad1e9c499e52e43cc46a30db1d1ee2ab975
maksimtoropov2111 <maksimtoropov2111@gmail.com>
2024-11-02 19:50:08 +0300

    main menu

---
+++ Assets/Scripts/AnketaScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+public class AnketaScript : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    public TMP_Text nameText;
+
+    public void OnButton() {
+        nameText.text = nameInputField.text;
+    }
+}
+using TMPro;[]{}    }TMP_InpurtField{};ppublicpupublicpup    public TMP_InputField nameInputField;
+    public TMP_Text nameText;
